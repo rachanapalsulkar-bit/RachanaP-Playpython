4e207422781e9e7b0e2944ea0d000abc8a99720e
Rachana Palsulkar <rachana.palsulkar@acldigital.com>
2026-06-24 15:42:55 +0530

    Day-4

--- Playwright+Python Assignment/Day-4test.py
+++ Playwright+Python Assignment/Day-4test.py
@@ -18,3 +18,21 @@ with sync_playwright() as p:
     print(page.locator("#password").get_attribute("placeholder"))
 
     page.context.browser.close()
+
+
+   #####################################################
+   #Perform login, validate success & error scenarios
+   #####################################################
+   
+from playwright.sync_api import sync_playwright
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.fill("#user-name", "standard_user")
+    page.fill("#password", "secret_sauce")
+    page.click("#login-button")
+
+    print("Current URL:", page.url)
+    page.wait_for_timeout(10000)  # Wait for 3 seconds
+    browser.close()
